430124f191d65c4cc77f73cf98a77925864825d1
maheeb <maheeb009@gmail.com>
2023-05-02 22:47:07 +0600

    primary setup done

---
+++ src/Component/Counter.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {useState} from "react";
+const Counter = () => {
+
+
+    return (
+        <h1>I am a counter</h1>
+    );
+};
+
+export default Counter;
---
+++ src/Component/Navigation.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navigation = () => {
+    return (
+        <div className='navbar-container'>
+            <h2>🔡 Word Counter</h2>
+        </div>
+    );
+};
+
+export default Navigation;
