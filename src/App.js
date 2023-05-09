5e37242cbb11f38b51125e300b66a148f7d54d32
maheeb <maheeb009@gmail.com>
2023-05-10 01:02:10 +0600

    removed unused code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import logo from './logo.svg';
+
 import './App.css';
-import {useState} from "react";
 
 import Counter from "./Component/Counter";
 import Navigation from "./Component/Navigation";
 
 function App() {
-  const [searchString, setSearchString] = useState('hi');
   return (
       <div className="App">
         <Navigation />
